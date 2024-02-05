c588796412b2696c6e39d0f0a8722ea09df47e43
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-02-05 13:00:26 +0200

    Update game.js

--- game.js
+++ game.js
@@ -24,8 +24,14 @@ function preload() {
 
 function create() {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
-    this.add.image(200, 100, 'star');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
     
 }
 
